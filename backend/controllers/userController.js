19450926e37e6e3073a021366f8f3373f4025cab
gourav <gourav@auvo.io>
2026-02-11 11:42:11 +0530

    feat: added a CRUD controllers, routes for user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,39 @@
 import asyncHandler from "express-async-handler";
 
-//@desc Authenticate user
+//@desc Authenticate user/ set token
 //@route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
     res.status(200).json({ message: "User authenticated" });
 });
 
-export { authUser };
+//@desc register a new user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
+    res.status(201).json({ message: "User registered" });
+});
+
+//@desc logout user
+//@route POST /api/users/logout
+//@access Public
+const logoutUser = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
+    res.status(200).json({ message: "User logged out" });
+});
+
+//@desc get user profile
+//@route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
+    res.status(200).json({ message: "User profile data" });
+});
+
+
+//@desc update user profile
+//@route PUT /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
+    res.status(200).json({ message: "User profile updated" });
+});
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -11,6 +12,9 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
     res.send('Server is ready')
 })
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
