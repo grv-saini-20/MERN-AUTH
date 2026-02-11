4f8a38e3a4feb5ed6324e5090fafce06d55f13a3
gourav <gourav@auvo.io>
2026-02-11 14:06:28 +0530

    feat: jwt added controller function added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,58 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 //@desc Authenticate user/ set token
 //@route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
-    res.status(200).json({ message: "User authenticated" });
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 //@desc register a new user
 //@route POST /api/users
 //@access Public
 const registerUser = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
-    res.status(201).json({ message: "User registered" });
+    const {name, email, password} = req.body;
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user) {
+        generateToken(res, user._id);
+        res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
 });
 
 //@desc logout user
 //@route POST /api/users/logout
 //@access Public
 const logoutUser = asyncHandler(async(req, res) => {              //asyncHandler helps to not wrap everything in try catch
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
     res.status(200).json({ message: "User logged out" });
 });
 
