c3e0a06edae488b13c1f795d950268fbb55fc4fa
gourav <gourav@auvo.io>
2026-02-11 15:30:53 +0530

    feat: auth middleware added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express  from "express";
 import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post('/', registerUser);
@@ -7,6 +8,6 @@ router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 // router.get('/profile', getUserProfile);
 // router.put('/profile', updateUserProfile);
-router.route('/profile').get(getUserProfile).put(updateUserProfile); //chaining
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile); //chaining
 
 export default router;
